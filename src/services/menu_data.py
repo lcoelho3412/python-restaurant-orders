9cd246e8afcbaaab505da6ebaf6f25501bce3fff
lcoelho3412 <leandrocoelho3@yahoo.com>
2023-05-24 20:58:49 -0300

    feat(Mapeamento): :poop: Implemente a classe MenuData
    faz o mapeamento de pratos e ingredientes baseado nos arquivo csv disponibilizado

--- src/services/menu_data.py
+++ src/services/menu_data.py
@@ -1,4 +1,22 @@
-# Req 3
+from models.dish import Dish
+from models.ingredient import Ingredient
+import pandas as pd
+
+
 class MenuData:
     def __init__(self, source_path: str) -> None:
-        pass
+        self.dishes = set()
+        self.data = pd.read_csv(source_path)
+
+        dishes = {}
+        for _, row in self.data.iterrows():
+            name, price, ingredient, amount = row
+
+            dish_data = dishes.get(name)
+            if dish_data is None:
+                dish_data = Dish(name, price)
+                dishes[name] = dish_data
+                self.dishes.add(dish_data)
+
+            ingredient_data = Ingredient(ingredient)
+            dish_data.add_ingredient_dependency(ingredient_data, amount)
