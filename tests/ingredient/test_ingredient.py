351ac4ba0c8334298ef0351805a6d3b6a4f6ea7b
lcoelho3412 <leandrocoelho3@yahoo.com>
2023-05-24 18:31:01 -0300

    Testa classes já implementadas

--- tests/ingredient/test_ingredient.py
+++ tests/ingredient/test_ingredient.py
@@ -1,6 +1,25 @@
-from src.models.ingredient import Ingredient  # noqa: F401, E261, E501
+from src.models.ingredient import Ingredient, Restriction
 
 
 # Req 1
 def test_ingredient():
-    pass
+    ingredient_name = "presunto"
+    ingredient2_name = "presunto"
+    ingredient3_name = "salmão"
+
+    ingredient = Ingredient(ingredient_name)
+    ingredient2 = Ingredient(ingredient2_name)
+    ingredient3 = Ingredient(ingredient3_name)
+
+    assert ingredient.name == ingredient_name
+    assert repr(ingredient) == f"Ingredient('{ingredient_name}')"
+
+    assert hash(ingredient) == hash(ingredient2)
+    assert hash(ingredient) != hash(ingredient3)
+    assert ingredient == ingredient2
+    assert ingredient != ingredient3
+
+    assert ingredient.restrictions == {
+        Restriction.ANIMAL_MEAT,
+        Restriction.ANIMAL_DERIVED,
+    }
